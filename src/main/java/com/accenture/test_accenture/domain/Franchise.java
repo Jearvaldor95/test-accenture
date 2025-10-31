9965e9fa0d1002583cc1f20b776831c4fac7884c
Jesús Valeta <jvaleta1995@gmail.com>
2025-10-30 19:24:38 -0500

    Modificando dominio

--- src/main/java/com/accenture/test_accenture/domain/Franchise.java
+++ src/main/java/com/accenture/test_accenture/domain/Franchise.java
@@ -1,10 +1,7 @@
 package com.accenture.test_accenture.domain;
 
-import java.util.List;
-
 public record Franchise(
     Long id,
-    String name,
-    List<Branch> branches
+    String name
 ) {
 }
